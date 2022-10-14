29644a588d4337bc9b15b6d9150146dcadff6995
JORGE LUIS DE FERRARI <joruge@gmail.com>
2022-10-14 20:16:32 -0300

    Ponteiros

---
+++ Estrutura de Dados com Algoritmos e C/1.1.3 Ponteiros/programa_ponteiro_03.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+ * Estrutura de Dados com Algoritmos e C, PDF pág 38
+ * Programa 1.7: Aritmética com ponteiros.
+ */
+
+int main()
+{
+
+    int *piValor;
+    int iValor;
+
+    char *pcValor;
+    char cValor;
+
+    piValor = &iValor;
+    pcValor = &cValor;
+
+
+    printf ("Endereco de piValor = %d\n", piValor);
+    printf ("Endereco de pcValor = %d\n", pcValor);
+
+    piValor++; /* somando uma unidade (4 bytes) na memória */
+    pcValor++; /* somando uma unidade (1 byte) na memória */
+    printf ("\nApós incremento de piValor e pcValor\n");
+    printf ("====================================\n");
+
+    
+    printf ("Endereco de piValor = %d\n", piValor);    
+    printf ("Endereco de pcValor = %d\n", pcValor);
+
+
+	return EXIT_SUCCESS;
+}
+
+
