535ea8ce1962696472c7072ef70fedc9ec24f80a
JORGE LUIS DE FERRARI <joruge@gmail.com>
2022-10-14 18:58:06 -0300

    Ponteiros

---
+++ Estrutura de Dados com Algoritmos e C/1.1.3 Ponteiros/programa_ponteiro_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+ * Estrutura de Dados com Algoritmos e C, PDF pág 36
+ * Programa 1.5: Exemplo de uso de ponteiros.
+ */
+
+
+int main()
+{
+
+	return EXIT_SUCCESS;
+}
