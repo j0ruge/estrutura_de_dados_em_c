d2a74a7e3a6a10248636a4f547c9d688d1bae534
JORGE FERRARI <joruge@gmail.com>
2022-10-18 18:13:02 -0300

    Usando agora visual studio

--- Aulas/Material Complementar/04_Funções/04_Funcoes/04_funcoes.c
+++ Aulas/Material Complementar/04_Funções/04_Funcoes/04_funcoes.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 
 void fatorial(int numero);
+int fatorial_v2(int numero);
 
 /* Função principal */
 int main(void)
 {
-	int numero;
-	scanf_s("%d", &numero);
+	int numero, resultado;
+	scanf_s("%d", &numero);	
 	fatorial(numero);
+
+	resultado = fatorial_v2(numero);
+
+	printf("Fatorial ver2 = %d\n", resultado);
 	return 0;
 }
 /* Função para imprimir o valor do fatorial */
@@ -20,5 +25,17 @@ void fatorial(int numero)
 	{
 		fatorial *= indice;
 	}		
-	printf("Fatorial = %d\n", fatorial);
+	printf("Fatorial ver1 = %d\n", fatorial);
+}
+
+/* programa que le um numero e imprime seu fatorial (versão 2) */
+int fatorial_v2(int numero)
+{
+	int indice;
+	int fatorial = 1;
+	for (indice = 1; indice <= numero; indice++)
+	{
+		fatorial *= indice;
+	}
+	return fatorial;
 }
