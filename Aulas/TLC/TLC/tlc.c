1a119a30a81a12c7c46e91376fc56d8820fd9b13
Jorge Luis de Oliveira Ferrari <20171104864@veigadealmeida.edu.br>
2022-10-20 20:09:12 -0300

    Adição de TLC

---
+++ Aulas/TLC/TLC/tlc.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lc {
+	int info;
+	struct lc* prox;
+}TLC;
+
+
+
+
+
+
+
+
+/*Libera - versão iterativa*/
+void libera(TLC* l) {
+	if (l) {
+		TLC* p = l->prox, * t;
+		while (p != l) {
+			t = p;
+			p = p->prox;
+			free(t);
+		}
+		free(l);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Inserir com complexidade O(1) - */
+
+TLC* insere_01(TLC* l, int x) {
+	TLC* novo = (TLC*)malloc(sizeof(TLC));
+	novo->info = x;
+	if (!l) {
+		novo->prox = novo;
+		return novo;
+	}
+	novo->prox = l->prox;
+	l->prox = novo;
+	return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+id imprime(TLC* l) {
+	if (l) {
+		printf("%d", l->prox);
+		TLC* p = l->prox;
+		while (p != l) {
+			printf("%d", p->info);
+			p = p->prox;
+		}
+
+	}
+}
+
+
+
+
+
+
+
+void ira(TLC* l, TLC* head) {
+	if (l != head) {
+		printf()
+	}
+}
+
+/* Imprime - versão recursiva */
+void imprime_rec(TLC* l) {
+	if (l) {
+		printf("%d ", l->info);
+		ira(l->prox, );
+	}
+}
