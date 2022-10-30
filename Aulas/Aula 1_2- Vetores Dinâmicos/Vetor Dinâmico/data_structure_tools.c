f083cfb39d30b474a64dc7f70f946f1dec9c1835
JORGE FERRARI <joruge@gmail.com>
2022-10-30 13:37:29 -0300

    Restruturação de pastas

---
+++ Aulas/Aula 1_2- Vetores Dinâmicos/Vetor Dinâmico/data_structure_tools.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "data_structure_tools.h"
+const int NOT_FOUND = -1;
+
+void imprimir_vetor(int* vetor_estatico, int quantidade_total_elementos) {
+	printf("\nTotal de elementos do vetor = %d", quantidade_total_elementos);
+	int indice;
+	for (indice = 0; indice < quantidade_total_elementos; indice++) {
+		printf("\nVET[%d] = %d", indice, vetor_estatico[indice]);
+	}
+}
+
+/*Objetivo: buscar um elemento no vetor
+entrada: vetor, a quantidade de elementos do vetor e o elemento a ser procurado
+saida: se encontrou o elemento, retornar o índice onde o mesmo está localizado.
+Caso contrário, retornar -1
+*/
+int buscar_elemento(int* vetor, int quantidade_elementos, int elemento) {
+	int indice_procurado;
+	int indice;
+	for (indice = 0; indice < quantidade_elementos; indice++) {
+		if (vetor[indice] == elemento) {
+			indice_procurado = indice;
+			return indice_procurado;
+		}
+	}
+	return NOT_FOUND;
+}
+
+int buscar_maior_elemento(int* vetor, int quantidade_elementos) {
+	int maior_elemento = vetor[0];
+	int indice;
+	for (indice = 1; indice < quantidade_elementos; indice++) {
+		if (vetor[indice] > maior_elemento) {
+			maior_elemento = vetor[indice];
+		}
+	}
+	return maior_elemento;
+}
+int buscar_menor_elemento(int* vetor, int quantidade_elementos) {
+	int menor_elemento = vetor[0];
+	int indice;
+	for (indice = 1; indice < quantidade_elementos; indice++) {
+		if (vetor[indice] < menor_elemento) {
+			menor_elemento = vetor[indice];
+		}
+	}
+	return menor_elemento;
+}
+
+/*TERMINAR DE REFATORAR*/
+void bubble_sort(int* vetor, int quantidade_elementos) {
+	int i, j, aux;
+
+	for (i = quantidade_elementos - 1; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (vetor[j] > vetor[j + 1]) {
+				aux = vetor[j];
+				vetor[j] = vetor[j + 1];
+				vetor[j + 1] = aux;
+			}
+		}
+	}
+}
---
+++ Aulas/Aula 1_2- Vetores Dinâmicos/Vetor Dinâmico/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <math.h>
+#include "..\Tools\data_structure_tools.h"
+#define NOT_FOUND -1
+
+#define FATOR_REALLOC 2
+
+/*OBJETIVO: Vetor Dinâmico*/
+
+
+int vetor_vazio(int* vetor);
+int* incluir_valor(int* vetor_dinamico, int valor, int* quantidade_elementos, int* quantidade_total_elementos);
+
+
+void informar_dados_vetor(int* vetor_estatico, int quantidade_elementos);
+
+
+void bubble_sort_v2(int* vetor_estatico, int quantidade_elementos);
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	int opcao;
+	int sair = 0;
+	int quantidade_elementos = 0;
+	int quantidade_total_elementos = 0;
+	int elemento, valor;
+
+	int* vetor_dinamico = NULL;
+
+
+	do {
+		system("cls");
+		printf("\n(1) Incluir numero");
+		printf("\n(2) Imprimir os elementos do vetor");
+		printf("\n(3) Buscar um elemento no vetor");
+		printf("\n(4) Buscar o maior elemento do vetor");
+		printf("\n(5) Buscar o menor elemento do vetor");
+		printf("\n(6) Ordenar o vetor");
+		printf("\n(7) Sair");
+		printf("\n\nDigite uma opcao: ");
+		scanf_s("%d", &opcao);
+
+		switch (opcao) {
+		case 1:
+			printf("\nInforme o valor a ser incluido: ");
+			scanf_s("%d", &valor);
+			vetor_dinamico = incluir_valor(vetor_dinamico, valor, &quantidade_elementos, &quantidade_total_elementos);
+			if (!vetor_dinamico) {
+				printf("\nO vetor estava cheio e nao foi possivel realoca-lo");
+			}
+			printf("\nquantidade de elementos = %d", quantidade_elementos);
+			__GETCHAR__;
+			break;
+		case 2:
+			imprimir_vetor(vetor_dinamico, quantidade_elementos);
+			__GETCHAR__;
+			break;
+		case 3:
+			printf("\nInforme o numero a ser procurado: ");
+			scanf_s("%d", &elemento);
+			int indice = buscar_elemento(vetor_dinamico, quantidade_elementos, elemento);
+			if (indice != NOT_FOUND) {
+				printf("Elemento encontrado na posicão %d", indice);
+			}
+			else {
+				printf("Elemento nao encontrado no vetor!");
+			}
+			__GETCHAR__;
+			break;
+
+		case 4:
+			printf("\nO maior elemento do vetor eh: %d", buscar_maior_elemento(vetor_dinamico, quantidade_elementos));
+			__GETCHAR__;
+			break;
+		case 5:
+			printf("\nO menor elemento do vetor eh: %d", buscar_menor_elemento(vetor_dinamico, quantidade_elementos));
+			__GETCHAR__;
+			break;
+		case 6:
+			bubble_sort(vetor_dinamico, quantidade_elementos);
+			break;
+
+		case 7:
+			printf("Sair");
+			sair = 1;
+			break;
+		default:
+			printf("Opcao Invalida!");
+			__GETCHAR__;
+			sair = 0;
+		}
+	} while (!sair);
+
+	return EXIT_SUCCESS;
+};
+
+int vetor_vazio(int* vetor) {
+	return vetor == NULL;
+}
+
+int* incluir_valor(int* vetor_dinamico, int valor, int* quantidade_elementos, int* quantidade_total_elementos) {
+	if (vetor_vazio(vetor_dinamico)) //ou poderia ser if(qtd==0) ou if(!qtd)
+	{
+		printf("\nO vetor estava vazio - primeira alocação!");
+		vetor_dinamico = (int*)malloc(FATOR_REALLOC * sizeof(int));
+		if (!vetor_dinamico) { //não conseguiu espaço em memória
+			return NULL;
+		}
+		*quantidade_total_elementos = 2;
+		*quantidade_elementos = 0;
+	}
+	if (*quantidade_elementos == *quantidade_total_elementos) { //meu vetor está cheio
+		printf("\nO vetor precisa de realocação!");
+		vetor_dinamico = (int*)realloc(vetor_dinamico, (*quantidade_total_elementos * FATOR_REALLOC) * sizeof(int));
+		if (!vetor_dinamico) { //não conseguiu espaço em memória
+			return NULL;
+		}
+		*quantidade_total_elementos = *quantidade_total_elementos * 2;
+	}
+	vetor_dinamico[*quantidade_elementos] = valor;
+	*quantidade_elementos = *quantidade_elementos + 1;
+	getchar();
+	return vetor_dinamico;
+}
